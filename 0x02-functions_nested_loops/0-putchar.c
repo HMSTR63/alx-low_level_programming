aa4a9f5d2abe49abd65065cdb8078202249f6d69
HMSTR63 <soufianejamali14@gmail.com>
2023-10-11 13:26:00 -0700

    oo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar using putchar prototype
+ *
+ * Return: Always 0 (Seccess)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_puthar(strp[]);
+	_putchar('\n');
+
+	return (0);
+}
