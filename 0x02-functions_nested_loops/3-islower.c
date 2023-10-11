9358086506c7214d6b1ce9e473ba1c2e68eeb56c
HMSTR63 <soufianejamali14@gmail.com>
2023-10-11 14:27:04 -0700

    oo

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islowr - function to check if hcaracter is lowercase
+ *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if 'c' is lowercase or always 0 (Seccess)
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
