69c51edc0b17474033a1858df8aa67bbb5eba52c
HMSTR63 <soufianejamali14@gmail.com>
2023-10-13 19:09:31 -0700

    ll

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,6 @@
  * more_numbers - print 0 - 14 ten times bay use _putchar
  *
  * Return: 0 (Seccess)
- *
 */
 
 void more_numbers(void);
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,24 +3,5 @@
 /**
  * print_diagonal - print a diagonal line
  *
- * @n: is the number of rimes the \ character should be print
- *
+ * @n: is the number of times the \ character should print
 */
-
-void print_diagonal(int n)
-{
-	int postn, space;
-
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		for (postn = 1; postn <= n; postn++)
-		{
-			for (postn = 1; posten <= postn; space++)
-				_putchar(' ');
-			_putchar(92); /* is equl to '/' char*/
-			_putchar('\n');
-		}
-	}
-}
