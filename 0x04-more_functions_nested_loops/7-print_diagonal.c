6ffd3788b568f0e04776d9f27310e37799b27ad5
HMSTR63 <soufianejamali14@gmail.com>
2023-10-13 19:15:10 -0700

    ll

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,3 +5,25 @@
  *
  * @n: is the number of times the \ character should print
 */
+
+void print_diagonal(int n)
+{
+	int row, space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= n; row++)
+		{
+			for (space = 1; space < row; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92); /* Backslash character */
+			_putchar('\n');
+		}
+	}
+}
