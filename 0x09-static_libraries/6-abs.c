e52751d1355bfdfdc83dc63545f7b64ae237bdb9
HMSTR63 <soufianejamali14@gmail.com>
2023-10-27 19:32:18 -0700

    hh

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - functin that computes the absolute value of an int
+ *
+ * @n: takes in int type input for function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
